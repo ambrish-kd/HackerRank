97ffdbf5ab704327dfad8ec2bd381f71ebd0fe79
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-08-04 23:28:33 +0530

    Add 'Hello_World.cpp' file

---
+++ HackerRank CPP/Hello_World.cpp
@@ -0,0 +1,10 @@
+// Problem Name:- Say "Hello, World!" With C++
+// Problem Link:- https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!";
+    return 0;
+}
