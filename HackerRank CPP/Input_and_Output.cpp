a9fbfd47978350bc59f00f8104f653a55149e81c
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-08-07 12:24:14 +0530

    Add Input_and_Output.cpp file

---
+++ HackerRank CPP/Input_and_Output.cpp
@@ -0,0 +1,12 @@
+// Problem Name:- Input and Output
+// Problem Link:- https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout << (num1 + num2 + num3);
+    return 0;
+}
