0f188b0d32f5bc9b9c918d3c6cb6d63a6216427b
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-08-10 23:53:01 +0530

    Add 'Conditional_Statements.cpp' file

---
+++ HackerRank CPP/Conditional_Statements.cpp
@@ -0,0 +1,17 @@
+// Problem Name:- Conditional Statements
+// Problem Link:- https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem?isFullScreen=true
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int in;
+  string num[11] = {"Greater than 9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","\0"};
+  cin >> in;
+  if(in > 9)
+    cout << num[0];
+  else
+    cout << num[in];
+  return 0;
+}
