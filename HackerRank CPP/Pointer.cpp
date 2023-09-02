9aa1aac89537826bf7e150f7268b82bb71036a4e
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-09-02 15:02:18 +0530

    Add 'Pointer.cpp' file

---
+++ HackerRank CPP/Pointer.cpp
@@ -0,0 +1,18 @@
+// Problem Name:- Pointer
+// Problem Link:- https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void update(int *a, int *b){
+    (*a) = (*a)+(*b);
+    (*b) = abs((*a)-2*(*b));   
+}
+
+int main(){
+    int a, b;
+    int *pa = &a, *pb = &b;
+    cin >> a >> b;
+    update(pa, pb);
+    cout << a << endl << b;
+}
