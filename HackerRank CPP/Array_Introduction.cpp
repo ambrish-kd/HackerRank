3314c69430b4a5d0f2356814c7640b043ca908ba
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-09-04 15:52:37 +0530

    Add 'Array_Introduction.cpp' file

---
+++ HackerRank CPP/Array_Introduction.cpp
@@ -0,0 +1,20 @@
+// Problem Name:- Array Introduction
+// Problem Link:- https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    // reverse(): reverse the vector
+    reverse(arr.begin(), arr.end());
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
