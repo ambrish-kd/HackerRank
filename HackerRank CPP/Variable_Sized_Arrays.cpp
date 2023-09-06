efa0ad3fb53b04546941b485293f22823eddfa42
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-09-06 17:18:03 +0530

    Add 'Variable_Sized_Arrays.cpp' file

---
+++ HackerRank CPP/Variable_Sized_Arrays.cpp
@@ -0,0 +1,30 @@
+// Problem Name:- Variable Sized Arrays
+// Problem Link:- https://www.hackerrank.com/challenges/variable-sized-arrays/problem?isFullScreen=true
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> arrays;
+    while(n--){
+        int k;
+        cin >> k;
+        vector<int> arr;
+        while(k--){
+            int a;
+            cin >> a;
+            arr.push_back(a);
+        }
+        arrays.push_back(arr);
+    }
+    
+    while(q--){
+        int arrNum, idx;
+        cin >> arrNum >> idx;
+        cout << arrays[arrNum][idx] << endl;
+    }
+    return 0;
+}
