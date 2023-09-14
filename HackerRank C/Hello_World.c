05365d03476c1dc4187ff778a9a616ddb660963e
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-09-14 15:09:29 +0530

    Add 'Hello_World.c' file

---
+++ HackerRank C/Hello_World.c
@@ -0,0 +1,10 @@
+// Problem Name:- "Hello, World!" in C
+// Problem Link:- https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include <stdio.h>
+
+int main() {
+    char string[50];
+    gets(string);
+    printf("Hello, World!\n%s", string);
+}
