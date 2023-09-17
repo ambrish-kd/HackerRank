96f698d335440408ad63d05684a80bc39f4771bf
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-09-17 17:39:06 +0530

    Add 'Python_If_Else.py' file

---
+++ HackerRank Python/Python_If_Else.py
@@ -0,0 +1,14 @@
+# Problem Name:- Python If-Else
+# Problem Link:- https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 == 1:
+        print("Weird")
+    else:
+        if n in range(2, 6):
+            print("Not Weird")
+        elif n in range(6, 21):
+            print("Weird")
+        elif n>20:
+            print("Not Weird")
