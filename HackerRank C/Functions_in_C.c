c2b99acb631e879d342b08fc163a81d71d8cad0e
Ambrish <dubey.ambrishdubey@gmail.com>
2023-09-30 23:34:54 +0530

    Add 'Functions_in_C.c' file

---
+++ HackerRank C/Functions_in_C.c
@@ -0,0 +1,24 @@
+// Problem Name:- Functions in C
+// Problem Link:- https://www.hackerrank.com/challenges/functions-in-c/problem?isFullScreen=true
+
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d){
+    if(a>b && a>c && a>d) {
+        return a;
+    } else if(b>a && b>c && b>d) {
+        return b;
+    } else if(c>a && c>b && c>d) {
+        return c;
+    } else {
+        return d;
+    }
+}
+
+int main(){
+    int num1, num2, num3, num4;
+    scanf("%d\n%d\n%d\n%d", &num1, &num2, &num3, &num4);
+    int max = max_of_four(num1, num2, num3, num4);
+    printf("%d",max);
+    return 0;
+}
