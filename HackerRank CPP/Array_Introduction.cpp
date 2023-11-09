ec7a551c426d6a197ab354a567b50d9d016dee5b
Ambrish <dubey.ambrishdubey@gmail.com>
2023-11-09 22:49:48 +0530

    Update Array_Introduction.cpp

--- HackerRank CPP/Array_Introduction.cpp
+++ HackerRank CPP/Array_Introduction.cpp
@@ -4,17 +4,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cin >> n;
+    cin>>n;
     vector<int> arr(n);
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
     }
-    // reverse(): reverse the vector
     reverse(arr.begin(), arr.end());
-    for(int i=0; i<n; i++){
-        cout << arr[i] << " ";
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
     }
-    return 0;
 }
