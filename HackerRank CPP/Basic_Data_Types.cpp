c76537e3aa3d87e9f7cc46cd0bda375a71afe96a
Ambrish <dubey.ambrishdubey@gmail.com>
2023-11-10 21:14:06 +0530

    Update Basic_Data_Types.cpp

--- HackerRank CPP/Basic_Data_Types.cpp
+++ HackerRank CPP/Basic_Data_Types.cpp
@@ -1,15 +1,17 @@
 // Problem Name:- Basic Data Types
 // Problem Link:- https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
 
-#include <iostream>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a; long b; char c; float d; double e;
+int main() {
+    int a; 
+    long b; 
+    char c; 
+    float d; 
+    double e;
     cin>>a>>b>>c>>d>>e;
     cout<<a<<endl<<b<<endl<<c<<endl;
     cout<<fixed<<setprecision(6)<<d<<endl;
     cout<<fixed<<setprecision(14)<<e<<endl; 
-    return 0;
 }
