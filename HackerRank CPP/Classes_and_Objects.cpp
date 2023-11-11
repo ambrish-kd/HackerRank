40b99d32a75ee3172e0f394ee451d7d7ee74e94d
Ambrish <dubey.ambrishdubey@gmail.com>
2023-11-11 21:55:52 +0530

    Update Classes_and_Objects.cpp

--- HackerRank CPP/Classes_and_Objects.cpp
+++ HackerRank CPP/Classes_and_Objects.cpp
@@ -1,10 +1,14 @@
 // Problem Name:- Classes and Objects
 // Problem Link:- https://www.hackerrank.com/challenges/classes-objects/problem?isFullScreen=true
 
+#include <bits/stdc++.h>
+using namespace std;
+
 class Student {
 private:
     int scores[5];
     int sum;
+
 public:
     Student() {
         sum = 0;
@@ -14,7 +18,7 @@ public:
     }
     void input() {
         for(int i=0; i<5; i++) {
-            cin>> scores[i];
+            cin>>scores[i];
             sum += scores[i];
         }
     }
