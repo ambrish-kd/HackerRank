5af4c78fd60e4df8b0b8dee416a34d6b4437cd26
Ambrish <dubey.ambrishdubey@gmail.com>
2023-11-15 23:15:08 +0530

    Update Input_and_Output.cpp

--- HackerRank CPP/Input_and_Output.cpp
+++ HackerRank CPP/Input_and_Output.cpp
@@ -1,12 +1,11 @@
 // Problem Name:- Input and Output
 // Problem Link:- https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int num1, num2, num3;
-    cin >> num1 >> num2 >> num3;
-    cout << (num1 + num2 + num3);
-    return 0;
+    cin>>num1>>num2>>num3;
+    cout<<(num1+num2+num3);
 }
