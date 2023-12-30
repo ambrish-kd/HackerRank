32683372336048459b3b3ec1c3a88dec3c096e72
Ambrish <dubey.ambrishdubey@gmail.com>
2023-12-30 19:45:20 +0530

    Add 'Write_a_function.py' file

---
+++ HackerRank Python/Write_a_function.py
@@ -0,0 +1,12 @@
+# Problem Name:- Write a function
+# Problem Link:- https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+
+def is_leap(year):
+    leap = False
+    if (year%4 == 0):
+        leap = True
+        if(year%100 == 0):
+            leap = False
+            if(year%400 == 0):
+                leap = True
+    return leap
