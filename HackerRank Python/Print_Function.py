2911e26f643a20df927aa8d47e7f805ab168a6df
Ambrish <dubey.ambrishdubey@gmail.com>
2023-12-31 22:02:01 +0530

    Add 'Print_Function.py' file

---
+++ HackerRank Python/Print_Function.py
@@ -0,0 +1,7 @@
+# Problem Name:- Print Function
+# Problem Link:- https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
