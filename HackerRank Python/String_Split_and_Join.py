76a73bf4974a7306ed379bd6bf9a0c55882e3637
Ambrish <dubey.ambrishdubey@gmail.com>
2024-01-09 18:45:52 +0530

    Add 'String_Split_and_Join.py' file

---
+++ HackerRank Python/String_Split_and_Join.py
@@ -0,0 +1,12 @@
+# Problem Name:- String Split and Join
+# Problem Link:- https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    # write your code here
+    line = '-'.join(line.split())
+    return line  
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
