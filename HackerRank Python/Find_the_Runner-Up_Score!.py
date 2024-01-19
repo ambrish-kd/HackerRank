b5fdf0ded35237c2d9509ea73bd75b3f2f4b94fd
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2024-01-19 19:36:53 +0530

    Update Find_the_Runner-Up_Score!.py

--- HackerRank Python/Find_the_Runner-Up_Score!.py
+++ HackerRank Python/Find_the_Runner-Up_Score!.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     # we have a map object arr that has the numbers so, remove duplicates by saving arr to a set
-    # set is unordered so to order it we need make it a list
+    # set is unordered so to order it we need to make it a list
     arr = list(set(arr))
     # now we sort the list
     arr = sorted(list(set(arr)))
-    # finally we print the runner up item which is the second last item of the sorted list
+    # finally we print the runner-up item which is the second last item of the sorted list
     print(arr[-2])
